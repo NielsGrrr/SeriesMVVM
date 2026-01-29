d28dac0996f76c056ea1dbc86d276ce74376336c
NielsGrrr <niels.stiefvater@etu.univ-smb.fr>
2026-01-29 10:48:14 +0100

    tests

--- SeriesMVVMTests/Services/WSServiceTests.cs
+++ SeriesMVVMTests/Services/WSServiceTests.cs
@@ -54,6 +54,53 @@ namespace SeriesMVVM.Services.Tests
         }
 
         [TestMethod()]
-        public 
+        public async Task GetSerieAsyncTest()
+        {
+            var result = await service.GetSerieAsync("series", 1);
+            Serie serie = new Serie
+            {
+                Serieid = 1,
+                Titre = "Scrubs",
+                Resume = "J.D. est un jeune médecin qui débute sa carrière dans l'hôpital du Sacré-Coeur. Il vit avec son meilleur ami Turk, qui lui est chirurgien dans le même hôpital. Très vite, Turk tombe amoureux d'une infirmière Carla. Elliot entre dans la bande. C'est une étudiante en médecine quelque peu surprenante. Le service de médecine est dirigé par l'excentrique Docteur Cox alors que l'hôpital est géré par le diabolique Docteur Kelso. A cela viennent s'ajouter plein de personnages hors du commun : Todd le chirurgien obsédé, Ted l'avocat dépressif, le concierge qui trouve toujours un moyen d'embêter JD... Une belle galerie de personnage !",
+                Nbsaisons = 9,
+                Nbepisodes = 184,
+                Anneecreation = 2001,
+                Network = "ABC (US)"
+            };
+            Assert.IsInstanceOfType(result, typeof(Serie));
+            Assert.AreEqual(serie, result, "Les séries ne sont pas identiques.");
+        }
+
+        [TestMethod]
+        public async Task PostSerieAsyncTest()
+        {
+            var nouvelleSerie = new Serie
+            {
+                Titre = "Test Unit MVP",
+                Resume = "Test de création",
+                Nbsaisons = 1
+            };
+
+            bool succes = await service.PostSerieAsync("series", nouvelleSerie);
+            Assert.IsTrue(succes, "L'ajout de la série a échoué.");
+        }
+
+        [TestMethod]
+        public async Task PutSerieAsyncTest()
+        {
+            var serieModifiee = new Serie
+            {
+                Serieid = 1,
+                Titre = "Scrubs Modifié",
+                Resume = "J.D. est un jeune médecin qui débute sa carrière dans l'hôpital du Sacré-Coeur. Il vit avec son meilleur ami Turk, qui lui est chirurgien dans le même hôpital. Très vite, Turk tombe amoureux d'une infirmière Carla. Elliot entre dans la bande. C'est une étudiante en médecine quelque peu surprenante. Le service de médecine est dirigé par l'excentrique Docteur Cox alors que l'hôpital est géré par le diabolique Docteur Kelso. A cela viennent s'ajouter plein de personnages hors du commun : Todd le chirurgien obsédé, Ted l'avocat dépressif, le concierge qui trouve toujours un moyen d'embêter JD... Une belle galerie de personnage !",
+                Nbsaisons = 9,
+                Nbepisodes = 184,
+                Anneecreation = 2001,
+                Network = "ABC (US)"
+            };
+            var result = await service.PutSerieAsync("series", serieModifiee.Serieid, serieModifiee);
+            Assert.IsInstanceOfType(result, typeof(bool));
+            Assert.IsTrue(result, "La modification de la série a échoué.");
+        }
     }
 }

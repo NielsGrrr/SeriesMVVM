1f3d82a8a1fb0d7e601bae91b6a3c3191f4b8335
NielsGrrr <niels.stiefvater@etu.univ-smb.fr>
2026-01-29 08:44:26 +0100

    Ajoutez des fichiers projet.

---
+++ SeriesMVVM/Services/IService.cs
@@ -0,0 +1,15 @@
+﻿using SeriesMVVM.Models.EntityFramework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SeriesMVVM.Services
+{
+    public interface IService
+    {
+        Task<List<Serie>> GetSeriesAsync(string nomControleur);
+        Task<Serie?> GetSerieAsync(string nomControleur, int id);
+        Task<bool> PutSerieAsync(string nomControleur, int id, Serie serie);
+        Task<Serie> PostSerieAsync(string nomControleur, Serie serie);
+        Task<bool> DeleteSerieAsync(string nomControleur, int id);
+    }
+}
---
+++ SeriesMVVM/Services/WSService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Protection.PlayReady;
+
+namespace SeriesMVVM.Services
+{
+    public class WSService : IService
+    {
+        private readonly HttpClient client = new HttpClient();
+        public WSService(string uri)
+        {
+            client.BaseAddress = new Uri(uri);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
